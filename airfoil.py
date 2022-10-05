953577b751f49ff3761f81463c345a76f181ed1d
OnePotatoCat <hengleon92@gmail.com>
2022-10-06 00:04:45 +0800

    Added airflow script and implementing it

---
+++ airfoil.py
@@ -0,0 +1,116 @@
+def naca4_symmetric ( t, c, x ):
+
+#*****************************************************************************80
+#
+## NACA4_SYMMETRIC evaluates y(x) for a NACA symmetric 4-digit airfoil.
+#
+#  Licensing:
+#
+#    This code is distributed under the GNU LGPL license.
+#
+#  Modified:
+#
+#    13 July 2018
+#
+#  Author:
+#
+#    John Burkardt
+#
+#  Reference:
+#
+#    Eastman Jacobs, Kenneth Ward, Robert Pinkerton,
+#    "The characteristics of 78 related airfoil sections from tests in
+#    the variable-density wind tunnel",
+#    NACA Report 460, 1933.
+#
+#  Parameters:
+#
+#    Input, real T, the maximum relative thickness.
+#
+#    Input, real C, the chord length.
+#
+#    Input, real X(*), points along the chord length.  
+#    0.0 <= X(*) <= C.
+#
+#    Output, real Y(*), for each value of X, the corresponding value of Y
+#    so that (X,Y) is on the upper wing surface, and (X,-Y) is on the
+#    lower wing surface.
+#
+  import numpy as np
+
+  y = 5.0 * t * c * ( \
+    0.2969 * np.sqrt ( x / c )\
+    + (((( \
+      - 0.1015 ) * ( x / c ) \
+      + 0.2843 ) * ( x / c ) \
+      - 0.3516 ) * ( x / c ) \
+      - 0.1260 ) * ( x / c ) )
+
+  return y
+
+def naca4_symmetric_test ( ):
+
+#*****************************************************************************80
+#
+## NACA4_SYMMETRIC_TEST tests NACA4_SYMMETRIC.
+#
+#  Licensing:
+#
+#    This code is distributed under the GNU LGPL license.
+#
+#  Modified:
+#
+#    13 July 2018
+#
+#  Author:
+#
+#    John Burkardt
+#
+  import matplotlib.pyplot as plt
+  import numpy as np
+
+  print ( '' )
+  print ( 'NACA4_SYMMETRIC_TEST' )
+  print ( '  NACA4_SYMMETRIC evaluates y(x) for a NACA' )
+  print ( '  symmetric airfoil defined by a 4-digit code.' )
+
+  c = 500.0
+  t = 0.25
+  n = 51
+  x = np.linspace ( 0.0, c, n )
+  x2 = np.append ( x, np.flip ( x, 0 ) )
+  y = naca4_symmetric ( t, c, x )
+  y2 = np.append ( y, np.flip ( -y, 0 ) )
+#
+#  Plot the wing surface.
+#
+  plt.plot ( x2, y2, 'b-', linewidth = 3 )
+  plt.axis ( 'equal' )
+  plt.grid ( True )
+  plt.xlabel ( '<---X--->', fontsize = 16 )
+  plt.ylabel ( '<---Y--->', fontsize = 16 )
+  plt.title ( 'NACA 4-digit symmetric airfoil', fontsize = 24 )
+
+  filename = 'naca4_symmetric_test.png'
+  plt.savefig ( filename )
+  print ( '' )
+  print ( '  Graphics saved in file "%s"' % ( filename ) )
+  plt.show ( )
+#
+#  Save data to a file.
+#
+  filename = 'naca4_symmetric_test.txt'
+  output = open ( filename, 'w' )
+  for i in range ( 0, 2 * n ):
+    s = '  %g  %g\n' % ( x2[i], y2[i] )
+    output.write ( s )
+  output.close ( )
+  print ( '  Data saved in file "%s"' % ( filename ) )
+
+  return
+
+if ( __name__ == '__main__' ):
+  from timestamp import timestamp
+  timestamp ( )
+  naca4_symmetric_test ( )
+  timestamp ( )
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 from math import sin
 from perlin_noise import PerlinNoise
 from collections import namedtuple, OrderedDict
+from airfoil import *
+import random
 
 width, height = 640, 640
-img_width, img_height = 480, 480
+img_width, img_height = 540, 540
 
 Color = namedtuple('RGB','red, green, blue')
 colors = {} #dict of colors
@@ -17,26 +19,52 @@ def hex_format(r, g, b):
 
 def main():
     window = Tk()
-    canvas = Canvas(window, bg="white", height=500, width=500)
+    canvas = Canvas(window, bg="white", height=height, width=width)
     canvas.pack()
 
     #Empty image
     img = PhotoImage(width=img_width, height=img_height)
-    # Perlin noise
-    noise = PerlinNoise(octaves=2, seed=12)
+    # Pattern 1 - Perlin noise
+    seed1 = random.randint(1,999999)
+    octave1 = random.randint(1,6)
+    noise1 = PerlinNoise(octaves=octave1, seed=seed1)
 
-    for x in range(img_width):
-        for y in range(img_height):
-            if int(200 * (noise([x/img_width, y/img_height]))) >= 15:
-                img.put("#%02x%02x%02x" % (200, 10, 10), (x, y))
-            else:
-                img.put("#%02x%02x%02x" % (255, 255, 255), (x, y))
+    # Pattern 2 - Perlin noise
+    seed2 = random.randint(1,999999)
+    octave2 = random.randint(1,6)
+    noise2 = PerlinNoise(octaves=octave2, seed=seed2)
+
+    threshold = random.randint(10,35)
+    thickness = random.uniform(0.185,0.355)
+
+    for x in range(img_width+1):
+        limit = (naca4_symmetric(thickness, img_width, x))
+        upper_limit =  limit + img_height/2
+        lower_limit = -limit + img_height/2
+
+        if x == img_width :
+            upper_limit = img_height/2
+            lower_limit = upper_limit
+
+        for y in range(img_height+1):
+
+            if y < upper_limit and y > lower_limit:
+                pigment1_intensity = int(200 * (noise1([x/img_width, y/img_height])))
+                pigment2_intensity = int(200 * (noise2([x/img_width, y/img_height])))
+
+                if pigment1_intensity >= pigment2_intensity and pigment1_intensity >= threshold:
+                    img.put("#%02x%02x%02x" % (200, 10, 10), (x, y))
+                elif pigment2_intensity >= pigment1_intensity and pigment2_intensity >= threshold:
+                    img.put("#%02x%02x%02x" % (55, 55, 55), (x, y))
+                else:
+                    img.put("#%02x%02x%02x" % (255, 255, 255), (x, y))
+
+            if y == int(upper_limit) or y == int(lower_limit):
+                img.put("#%02x%02x%02x" % (0, 0, 0), (x, y))
 
 
-    canvas.create_image((640-480)/2, (640-480)/2, image=img, anchor=NW)
+    canvas.create_image((width-img_width)/2, (height-img_height)/2, image=img, anchor=NW)
     mainloop()
-
-
 
 
 if __name__== "__main__":
