fd12be974f2fc7cb259a3af4dcd5d638cf0502aa
OnePotatoCat <2301123Leon>
2022-10-14 14:31:20 +0800

    Revised width, height to global variable

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from turtle import width
 from perlin_noise import PerlinNoise
 from airfoil import *
 import math
@@ -16,10 +15,16 @@ white = np.array((255, 255, 255, 255))
 transparent = np.array((255, 255, 255, 0))
 eyeblack = np.array((0, 0, 0, 255))
 
-def main():
-    width = 1280
-    height = 1280
+WIDTH = 600
+HEIGTH = 600
+EYE_WIDTH = 60
+EYE_THICKNESS = 25
 
+KOI_THICKNESS_MAX = 0.35
+KOI_THICKNESS_MIN = 0.18
+
+
+def main():
     # Pattern 1 - Perlin noise
     seed1 = random.randint(1,999999)
     octave1 = random.randint(1,6)
@@ -30,45 +35,45 @@ def main():
     octave2 = random.randint(1,6)
     noise2 = PerlinNoise(octaves=octave2, seed=seed2)
 
-    eye_x = int(random.uniform(0.05, 0.15)*width)
-    eye_width = 60
-    eye_thickness = 25
+    eye_x = int(random.uniform(0.035, 0.125)*WIDTH)
+    EYE_WIDTH = 22
+    EYE_THICKNESS = 10
     threshold = random.uniform(-0.2, 0.3)
-    thickness = random.uniform(0.185,0.355)
+    thickness = random.uniform(0.185,0.3)
 
-    x1 = np.linspace(0, width, width*4)
+    x1 = np.linspace(0, WIDTH, WIDTH*4)
     x2 = np.append(x1, np.flip(x1, 0))
 
-    y1 = naca4_symmetric(thickness, width, x1)
+    y1 = naca4_symmetric(thickness, WIDTH, x1)
     y2 = np.append(y1, np.flip(-y1, 0))
-    y2 = y2 + height/2
+    y2 = y2 + HEIGTH/2
 
-    eyelayer = np.full([height, width, 4], transparent)
-    pig1 = np.empty([height, width, 4], dtype = int)
-    pig2 = np.empty([height, width, 4], dtype = int)
+    eyelayer = np.full([WIDTH, HEIGTH, 4], transparent)
+    pig1 = np.empty([WIDTH, HEIGTH, 4], dtype = int)
+    pig2 = np.empty([WIDTH, HEIGTH, 4], dtype = int)
     
-    for x in range(width):
-        limit = (naca4_symmetric(thickness, width, x))
-        upper_limit =  limit + height/2
-        lower_limit = -limit + height/2
+    for x in range(WIDTH):
+        limit = (naca4_symmetric(thickness, WIDTH, x))
+        upper_limit =  limit + HEIGTH/2
+        lower_limit = -limit + HEIGTH/2
 
-        for y in range(height):
+        for y in range(HEIGTH):
             if x == eye_x:
                 if y == int(upper_limit):
-                    for i in range(eye_width):
-                        for j in range(int(eye_thickness*math.sin(math.radians(2.8*i)))):
-                            temp_limit = naca4_symmetric(thickness, width, x+i) + height/2
+                    for i in range(EYE_WIDTH):
+                        for j in range(int(EYE_THICKNESS*math.sin(math.radians(180*i/EYE_WIDTH)))):
+                            temp_limit = naca4_symmetric(thickness, WIDTH, x+i) + HEIGTH/2
                             eyelayer[int(temp_limit) - j, x+i] = eyeblack
 
                 elif y == int(lower_limit):
-                    for i in range(eye_width):
-                        for j in range(int(eye_thickness*math.sin(math.radians(2.8*i)))):
-                            temp_limit = -naca4_symmetric(thickness, width, x+i) + height/2
+                    for i in range(EYE_WIDTH):
+                        for j in range(int(EYE_THICKNESS*math.sin(math.radians(180*i/EYE_WIDTH)))):
+                            temp_limit = -naca4_symmetric(thickness, WIDTH, x+i) + HEIGTH/2
                             eyelayer[int(temp_limit) + j, x+i] = eyeblack
             
             if y > lower_limit and y < upper_limit:
-                pigment1_intensity = (noise1([x/width, y/height]))
-                pigment2_intensity = (noise2([x/width, y/height]))
+                pigment1_intensity = (noise1([x/WIDTH, y/HEIGTH]))
+                pigment2_intensity = (noise2([x/WIDTH, y/HEIGTH]))
 
                 if pigment1_intensity > pigment2_intensity > threshold:
                     pig1[y, x] = orange1
@@ -93,8 +98,8 @@ def main():
 
     koi = plt.figure()
     shape = koi.add_subplot(111)
-    shape.plot(x2, y2, 'k-', linewidth=0.5)
-    shape.axis([0, width, 0, height])
+    shape.plot(x2, y2, 'k-', lw=0.5)
+    shape.axis([0, WIDTH, 0, HEIGTH])
     shape.axis('off')
     
     shape.imshow(pig1)
@@ -103,6 +108,8 @@ def main():
     plt.show() 
     #koi.savefig("koi.png", transparent=True)
 
+def layering():
+    return
 
 if __name__== "__main__":
     main()
