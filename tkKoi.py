a4491c4a62b28dc8dc2eec6d900acf76bb558c6d
OnePotatoCat <hengleon92@gmail.com>
2022-10-18 01:00:12 +0800

    Fixed tkKoi create_image method

--- tkKoi.py
+++ tkKoi.py
@@ -6,8 +6,9 @@ class tkKoi:
     def __init__(self, master : tk.Tk, canvas : tk.Canvas, x : int, y : int, image : str):
         self.master = master
         self.canvas = canvas
-        temp_image = Image.open(image)
-        #temp_image = temp_image.resize((100, 100))
-        koi_image = ImageTk.PhotoImage(file=image, master = canvas)
-        self.image = self.canvas.create_image(10, 10, image=koi_image)
-        self.image2 = canvas.create_oval(10,10,50,50,fill="white")
+        self.temp_image = Image.open(image)
+        self.temp_image = self.temp_image.resize((100, 100))
+
+        self.koi_image = ImageTk.PhotoImage(self.temp_image, master = self.master)
+        self.image = self.canvas.create_image(x, y, image=self.koi_image)
+
